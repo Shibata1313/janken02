a3ef1f2fdf81ce91f8374ed5ef714ebd8b7fd276
Shibata1313 <e1b21047@oit.ac.jp>
2023-10-03 14:31:04 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1790/kaizai/janken02/Janken02Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1790.kaizai.janken02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1790/kaizai/janken02/Janken02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1790.kaizai.janken02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
